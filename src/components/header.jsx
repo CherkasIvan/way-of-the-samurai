33ce85c09f5f82e8fadf8e99da51507a0efcb47f
CherkasIvan <distor6on@yandex.ru>
2023-06-07 15:42:33 +0300

    first commit

---
+++ src/components/header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <ul className="header-list">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Main</a>
+        </li>
+        <li>
+          <a href="#">Nav</a>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/technologies.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Technologies = () => {
+  return (
+    <>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JS</li>
+        <li>React</li>
+      </ul>
+    </>
+  );
+};
+
+export default Technologies;
