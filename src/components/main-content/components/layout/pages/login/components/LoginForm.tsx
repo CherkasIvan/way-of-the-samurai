76fdd3f8aa5e6e0b5cc088b3b60eb619f026c2d2
CherkasIvan <distor6on@yandex.ru>
2023-09-03 09:31:35 +0300

    add validation

--- src/components/main-content/components/layout/pages/login/components/LoginForm.tsx
+++ src/components/main-content/components/layout/pages/login/components/LoginForm.tsx
@@ -1,18 +1,36 @@
 import React, { FC } from 'react'
 import styles from './LoginForm.module.scss'
 import { Field, reduxForm } from 'redux-form'
+import { Input } from '../../../../../../shared/forms-controls/FormsControls'
+import { maxLengthCreator, requiredField } from '../../../../../../../utils/validators/validators'
 
-const LoginForm= (props:any) => {
+const maxLength20 = maxLengthCreator(20)
+const maxLength10 = maxLengthCreator(10)
+const required = requiredField
+
+const LoginForm = (props: any) => {
   return (
     <form onSubmit={props.handleSubmit}>
       <div>
-        <Field type='text' placeholder={'LOGIN'} name={'login'} component={'input'}/>
+        <Field
+          type='text'
+          placeholder={'LOGIN'}
+          name={'login'}
+          component={Input}
+          validate={[required, maxLength20]}
+        />
       </div>
       <div>
-        <Field type='password' placeholder={'PASSWORD'} name={'password'} component={'input'}/>
+        <Field
+          type='password'
+          placeholder={'PASSWORD'}
+          name={'password'}
+          component={Input}
+          validate={[required, maxLength10]}
+        />
       </div>
       <div>
-        <Field type='checkbox' component={'input'} name={'rememberMe'}/>
+        <Field type='checkbox' component={'input'} name={'rememberMe'} />
         <span>Remember me</span>
       </div>
       <div>
@@ -22,6 +40,6 @@ const LoginForm= (props:any) => {
   )
 }
 
-const LoginReduxForm = reduxForm({ form: 'login' }) (LoginForm)
+const LoginReduxForm = reduxForm({ form: 'login' })(LoginForm)
 
 export default LoginReduxForm
